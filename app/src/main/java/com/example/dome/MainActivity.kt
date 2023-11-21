1ae7453330473fdb63b676454b75557936ec3dd9
PRIV4TTE <eco201189@gmail.com>
2023-11-20 22:42:25 -0500

    action FAB

--- app/src/main/java/com/example/dome/MainActivity.kt
+++ app/src/main/java/com/example/dome/MainActivity.kt
@@ -1,41 +1,44 @@
 package com.example.dome
 
 import android.os.Bundle
-import android.widget.Toast
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.CheckBox
+import android.widget.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var titleTextView: TextView
-    private lateinit var itemTextView: TextView
-    private lateinit var itemCheckBox: CheckBox
 
-    private lateinit var backButton: ImageButton
-    private lateinit var alarmButton: ImageButton
-    private lateinit var settingsButton: ImageButton
+    private lateinit var tasksContainer: LinearLayout
+    private lateinit var addTaskButton: FloatingActionButton
+    private lateinit var titleTextView: TextView
+    private lateinit var itemTextView: EditText
+    private lateinit var itemCheckBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.item)
 
-        // Obtener referencias a los elementos de la interfaz
-        titleTextView = findViewById<TextView>(R.id.titleTextView)
-        itemTextView = findViewById<TextView>(R.id.taskEditText)
-        itemCheckBox = findViewById<CheckBox>(R.id.itemCheckBox)
+        addTaskButton = findViewById(R.id.addTaskButton)
+        titleTextView = findViewById(R.id.titleTextView)
+        itemTextView = findViewById(R.id.taskEditText)
+        itemCheckBox = findViewById(R.id.itemCheckBox)
+        tasksContainer = findViewById(R.id.tasksContainer)
 
-        backButton = findViewById<ImageButton>(R.id.backButton)
-        alarmButton = findViewById<ImageButton>(R.id.alarmButton)
-        settingsButton = findViewById<ImageButton>(R.id.settingsButton)
+        addTaskButton.setOnClickListener {
+            agregarNuevaTarea()
+        }
 
-        // Establecer los textos y el estado del CheckBox
         titleTextView.text = "DoMe"
         itemCheckBox.isChecked = false
 
-        // Configurar el botón de retroceso
+        val backButton: ImageButton = findViewById(R.id.backButton)
+        val alarmButton: ImageButton = findViewById(R.id.alarmButton)
+        val settingsButton: ImageButton = findViewById(R.id.settingsButton)
+
         backButton.setOnClickListener {
-            onBackPressed() // Llama al método onBackPressed para simular el botón de retroceso
+            onBackPressed()
         }
 
         alarmButton.setOnClickListener {
@@ -45,19 +48,57 @@ class MainActivity : AppCompatActivity() {
         settingsButton.setOnClickListener {
             // Lógica para el botón de configuraciones
         }
-
-        // agregar más lógica a tu aplicación
     }
 
-    // Manejo del botón de retroceso de la actividad
     override fun onBackPressed() {
         finish()
-        super.onBackPressed() // Llama a la implementación predeterminada de onBackPressed si es necesario
+        super.onBackPressed()
     }
 
-        private fun showAlarmMessage() {
-            // Por ejemplo, mostrar un Toast sencillo
-            Toast.makeText(this, "¡Alarma configurada!", Toast.LENGTH_SHORT).show()
+    private fun showAlarmMessage() {
+        Toast.makeText(this, "¡Alarma configurada!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun agregarNuevaTarea() {
+        val tarea = itemTextView.text.toString().trim()
+
+        if (tarea.isNotEmpty()) {
+            val nuevaTareaLayout = LinearLayout(this)
+            nuevaTareaLayout.orientation = LinearLayout.HORIZONTAL
+
+            val nuevaTareaEditText = EditText(this)
+            nuevaTareaEditText.layoutParams =
+                LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f)
+            nuevaTareaEditText.hint = "Tarea"
+            nuevaTareaEditText.textSize = 16f
+            nuevaTareaEditText.setPadding(16, 16, 16, 16)
+            nuevaTareaEditText.text = Editable.Factory.getInstance().newEditable(tarea)
+
+            val nuevaCheckBox = CheckBox(this)
+            nuevaCheckBox.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+
+            nuevaTareaLayout.addView(nuevaTareaEditText)
+            nuevaTareaLayout.addView(nuevaCheckBox)
+
+            tasksContainer.addView(nuevaTareaLayout)
+            itemTextView.text.clear()
+        } else {
+            Toast.makeText(this, "Ingrese una tarea", Toast.LENGTH_SHORT).show()
         }
     }
+
+}
+
+
+
+
+
+
+
+
+
+
 
