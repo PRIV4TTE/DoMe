3d57029f960972047f1a3a521d7865c4ab4d2d50
PRIV4TTE <eco201189@gmail.com>
2023-12-06 16:01:27 -0500

    Logica para eliminación de una tarea cuando se marca la casilla de verificación

--- app/src/main/java/com/example/dome/MainActivity.kt
+++ app/src/main/java/com/example/dome/MainActivity.kt
@@ -54,62 +54,7 @@ class MainActivity : AppCompatActivity() {
 
         alarmButton.setOnClickListener {
             showAlarmMessage()
-            override fun onBackPressed() {
-                finish()
-                super.onBackPressed()
-            }
-
-            private fun showAlarmMessage() {
-                Toast.makeText(this, "¡Alarma configurada!", Toast.LENGTH_SHORT).show()
-            }
-// ... Tu código anterior ...
-
-            private fun agregarNuevaTarea() {
-                val tarea = itemTextView.text.toString().trim()
-
-                if (tarea.isNotEmpty()) {
-                    val nuevaTareaLayout = LinearLayout(this)
-                    nuevaTareaLayout.orientation = LinearLayout.HORIZONTAL
-
-                    val nuevaTareaEditText = EditText(this)
-                    nuevaTareaEditText.layoutParams =
-                        LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f)
-                    nuevaTareaEditText.hint = "Tarea"
-                    nuevaTareaEditText.textSize = 22f
-                    nuevaTareaEditText.setPadding(18, 18, 18, 18)
-                    nuevaTareaEditText.setBackgroundColor(Color.TRANSPARENT)
-                    nuevaTareaEditText.text = Editable.Factory.getInstance().newEditable(tarea)
-
-                    val nuevaCheckBox = CheckBox(this)
-                    nuevaCheckBox.layoutParams = LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.WRAP_CONTENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT
-                    )
-
-                    nuevaCheckBox.setOnCheckedChangeListener { _, isChecked ->
-                        if (isChecked) {
-                            eliminarTarea(nuevaTareaLayout)
-                        }
-                    }
-
-                    nuevaTareaLayout.addView(nuevaTareaEditText)
-                    nuevaTareaLayout.addView(nuevaCheckBox)
-
-                    tasksContainer.addView(nuevaTareaLayout)
-                    itemTextView.text.clear()
-                } else {
-                    Toast.makeText(this, "Ingrese una tarea", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            private fun eliminarTarea(layout: LinearLayout) {
-                tasksContainer.removeView(layout)
-            }
-
         }
-
-
-    }
 
         settingsButton.setOnClickListener {
             // Lógica para el botón de configuraciones
@@ -117,3 +62,58 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    override fun onBackPressed() {
+        finish()
+        super.onBackPressed()
+    }
+
+    private fun showAlarmMessage() {
+        Toast.makeText(this, "¡Alarma configurada!", Toast.LENGTH_SHORT).show()
+    }
+// ... Tu código anterior ...
+
+    private fun agregarNuevaTarea() {
+        val tarea = itemTextView.text.toString().trim()
+
+        if (tarea.isNotEmpty()) {
+            val nuevaTareaLayout = LinearLayout(this)
+            nuevaTareaLayout.orientation = LinearLayout.HORIZONTAL
+
+            val nuevaTareaEditText = EditText(this)
+            nuevaTareaEditText.layoutParams =
+                LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f)
+            nuevaTareaEditText.hint = "Tarea"
+            nuevaTareaEditText.textSize = 22f
+            nuevaTareaEditText.setPadding(18, 18, 18, 18)
+            nuevaTareaEditText.setBackgroundColor(Color.TRANSPARENT)
+            nuevaTareaEditText.text = Editable.Factory.getInstance().newEditable(tarea)
+
+            val nuevaCheckBox = CheckBox(this)
+            nuevaCheckBox.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+
+            nuevaCheckBox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    eliminarTarea(nuevaTareaLayout)
+                }
+            }
+
+            nuevaTareaLayout.addView(nuevaTareaEditText)
+            nuevaTareaLayout.addView(nuevaCheckBox)
+
+            tasksContainer.addView(nuevaTareaLayout)
+            itemTextView.text.clear()
+        } else {
+            Toast.makeText(this, "Ingrese una tarea", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun eliminarTarea(layout: LinearLayout) {
+        tasksContainer.removeView(layout)
+    }
+
+}
+
